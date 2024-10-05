1b386cfe321db7c09e09bfb4be6e6b0e1fffdd3b
Candela Landi <landicandela01@gmail.com>
2024-10-05 01:27:49 -0300

    SPA control

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 
+const path = require('path');
+
 const cors = require('cors');
 
 const { dbConnection } = require('./database/config');
@@ -29,6 +31,10 @@ app.use( '/api/doctors', require('./routes/doctors') );
 app.use( '/api/all', require('./routes/searches') );
 app.use( '/api/login', require('./routes/auth') );
 app.use( '/api/uploads', require('./routes/uploads') );
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
 
 
 app.listen( process.env.PORT, () => {
