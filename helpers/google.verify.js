86b1c861ef78feec06ab3d9bf5e82622d928aeb6
Candela Landi <landicandela01@gmail.com>
2024-11-09 18:16:51 -0300

    front changes

--- helpers/google.verify.js
+++ helpers/google.verify.js
@@ -4,9 +4,7 @@ const client = new OAuth2Client(process.env.GOOGLE_SECRET);
 async function googleVerify( token ) {
   const ticket = await client.verifyIdToken({
     idToken: token,
-    audience: process.env.GOOGLE_ID, // Specify the CLIENT_ID of the app that accesses the backend
-    // Or, if multiple clients access the backend:
-    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    audience: process.env.GOOGLE_ID
   });
   const payload = ticket.getPayload();
   console.log({ payload });
