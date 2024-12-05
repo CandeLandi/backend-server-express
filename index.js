99539be9e0963509132db8fc36d849440ea2ab70
Candela Landi <landicandela01@gmail.com>
2024-12-05 09:33:15 -0300

    add headers

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const corsOptions = {
     },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
-    allowedHeaders: ['Content-Type', 'Authorization'] 
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-token'] 
 };
 
 app.use(cors(corsOptions));
